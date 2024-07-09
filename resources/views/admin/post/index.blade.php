37591808048d4dab568de389411fcc14fd77d1f4
Corkerro <74060229+Corkerro@users.noreply.github.com>
2024-07-09 23:07:14 +0300

    Lesson 31. Admin LTE в Laravel, устанавливаем админку

---
+++ resources/views/admin/post/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin')
+
+@section('content')
+    <a class="btn btn-primary mt-3 px-lg-5" href="{{route('post.create')}}">Create</a>
+    <div>
+        @foreach($posts as $post)
+            <div class="mt-lg-5">
+                <h2><a href="{{ route('post.show', $post->id) }}">{{ $post->id }}. {{ $post->title }}</a></h2>
+                <p>{{ $post->content }}</p>
+            </div>
+        @endforeach
+    </div>
+
+    <div>
+        {{ $posts->withQueryString()->links() }}
+    </div>
+
+@endsection
