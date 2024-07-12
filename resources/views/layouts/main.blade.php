ed9f0b26364f7c32875357bd357d32103db8b1b6
Corkerro <74060229+Corkerro@users.noreply.github.com>
2024-07-12 21:20:57 +0300

    Lesson 34. Класс Policy в Laravel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,32 +9,38 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="{{ route('main.index') }}">Main</a>
-                        </li>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('main.index') }}">Main</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                    </li>
+                    @can('view', auth()->user())
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                            <a class="nav-link" href="{{ route('admin.post.index') }}">Admin</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('about.index') }}">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-        @yield('content')
+                    @endcan
+                </ul>
+            </div>
+        </nav>
     </div>
+    @yield('content')
+</div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome')->name('home');
-});
-
 
 Route::group(['namespace' => 'Post'], function () {
     Route::get('/posts', 'IndexController')->name('post.index');
